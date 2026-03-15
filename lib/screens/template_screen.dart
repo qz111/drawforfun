06dbb0b4d32c6724843d6a309b441af7613f0d32
qz111 <kloseqz@gmail.com>
2026-03-15 12:32:02 +0100

    fix: use withValues(alpha:) instead of deprecated withOpacity in TemplateScreen

--- lib/screens/template_screen.dart
+++ lib/screens/template_screen.dart
@@ -59,7 +59,7 @@ class _AnimalTemplateCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.08),
+              color: Colors.black.withValues(alpha: 0.08),
               blurRadius: 6,
               offset: const Offset(0, 2),
             ),
